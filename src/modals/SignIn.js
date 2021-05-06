bd2e792bf6557209ee05595b9bd6eb34b1f3e40d
kiss8981 <kdh89811@gmail.com>
2021-05-06 20:47:21 +0800

    프론드 엔드 디자인

--- src/modals/SignIn.js
+++ src/modals/SignIn.js
@@ -1,11 +1,5 @@
 import React from "react";
-import { Modal, Button, Form, Container } from "react-bootstrap";
-import { GoogleLogin } from "react-google-login";
-import HorizontalLine from "../components/HorizontalLine";
-
-const responeGoogle = res => {
-    console.log(res)
-}
+import { Modal, Button, Form, Container, Alert } from "react-bootstrap";
 
 const SignInModal = ({ show, onHide }) => {
   return (
@@ -18,40 +12,32 @@ const SignInModal = ({ show, onHide }) => {
     >
       <Container>
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">회원가입</Modal.Title>
+          <Modal.Title id="contained-modal-title-vcenter">로그인</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+        <Form className="FormUser">
+            <Alert variant="danger">
+                <i class="fas fa-exclamation-circle"></i>  백엔드 서버 개발중입니다!
+            </Alert>
             <Form.Group>
-              <Form.Label>이메일</Form.Label>
-              <Form.Control type="email" placeholder="이메일" />
+                <Form.Label><i class="fas fa-envelope"/> 이메일</Form.Label>
+                <Form.Control type="email" placeholder="이메일"/>
             </Form.Group>
 
             <Form.Group>
-              <Form.Label>비밀번호</Form.Label>
-              <Form.Control type="password" placeholder="비밀번호" />
+                <Form.Label><i class="fas fa-lock"/> 비밀번호</Form.Label>
+                <Form.Control type="password" placeholder="비밀번호"/>
             </Form.Group>
             <Button block type="button" className="social-btn my-3">
-              로그인
+            <i class="fas fa-sign-in-alt"></i>&nbsp; 로그인
             </Button>
-            <HorizontalLine />
-            <GoogleLogin
-              clientId="183101622325-9e3rckitc7jt7ienvkva4q92j1okkkel.apps.googleusercontent.com"
-              onSuccess={responeGoogle}
-              onFailure={responeGoogle}
-              render={(renderProps) => {
-                return (
-                  <Button block type="button" className="social-btn" onClick={renderProps.onClick}
-                  ><i className="fab fa-google"></i>&nbsp; 구글 로그인
-                  </Button>
-                );
-              }}
-            />
-            <Form.Text>
-                asdaasd
-            </Form.Text>
-            
-          </Form>
+            <Button type="button" className="social-btn mr-1">
+            <i class="fab fa-google"></i>&nbsp;구글
+            </Button>
+            <Button type="button" className="social-btn ml-1">
+            <i class="fas fa-user-circle"></i>&nbsp;네이버
+            </Button>
+            </Form>
         </Modal.Body>
       </Container>
     </Modal>
--- src/modals/SignUp.js
+++ src/modals/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Modal, Button, Form, Container } from "react-bootstrap";
+import { Modal, Button, Form, Container, Alert } from "react-bootstrap";
 import PasswordStrengthMeter from '../components/passwordcheck';
 
 
@@ -18,24 +18,32 @@ const SignUpModal = ({ show, onHide }) => {
           <Modal.Title id="contained-modal-title-vcenter">회원가입</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+            
+          <Form className="FormUser">
+          <Alert variant="danger">
+                <i class="fas fa-exclamation-circle"></i>  백엔드 서버 개발중입니다!
+            </Alert>
             <Form.Group>
-              <Form.Label>이름</Form.Label>
-              <Form.Control placeholder="이름" />
+                <Form.Label><i class="fas fa-envelope"></i> 이메일</Form.Label>
+                <Form.Control type="email" placeholder="이메일"/>
             </Form.Group>
-
             <Form.Group>
-              <Form.Label>이메일</Form.Label>
-              <Form.Control type="email" placeholder="이메일" />
+                <Form.Label><i class="fas fa-user"></i> 이름</Form.Label>
+                <Form.Control type="text" placeholder="이름"/>
             </Form.Group>
-
             <Form.Group>
               <Form.Label>비밀번호</Form.Label>
-              <Form.Control name="user-password" type="password" onChange={e => setPassword(e.target.value)}/>
+              <Form.Control name="user-password" type="password" placeholder="비밀번호" onChange={e => setPassword(e.target.value)}/>
               <PasswordStrengthMeter password={password} />
             </Form.Group>
             <Button block type="button" className="social-btn my-3">
-              회원가입
+            <i class="fas fa-sign-in-alt"></i>&nbsp; 회원가입
+            </Button>
+            <Button type="button" className="social-btn mr-1">
+            <i class="fab fa-google"></i>&nbsp;구글
+            </Button>
+            <Button type="button" className="social-btn ml-1">
+            <i class="fas fa-user-circle"></i>&nbsp;네이버
             </Button>
           </Form>
         </Modal.Body>
